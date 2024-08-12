4822dc989baadfd0b390ca5967a9188a4f50e25c
Shreyans298 <jain.shreyans03@gmail.com>
2024-08-13 03:05:34 +0530

    Creating /contact route and managing html page to be rendered

--- contact.php
+++ contact.php
@@ -4,17 +4,29 @@
     <title></title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
     />
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="styles.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Sriracha&display=swap" rel="stylesheet">
 
   </head>
 
 <body>
-<?php include 'navbar.php'; ?>
+<div id="page1">
+     <nav>
+        <img src="logo.png" alt="pic">
+        <h1 class="company">SATTYA SPACE SOLUTIONS</h1>
+        <div id="nav-part2">
+            <h4><a href="index.php">Home</a></h4>
+            <h4><a href="about.php">About</a></h4>
+            <h4><a href="blog.php">Blog</a></h4>
+            <h4><a href="contact.php">Contact Us</a></h4>
+        </div>
+    </nav>
+</div>
 <section class="my-5">
   <div>
     <h3 class="text-center">Our Services</h3>
@@ -41,7 +53,23 @@
 </div>
 </div>
 </section>
-
-<?php include 'footer.php'; ?>
+<footer>
+      <div class="footer-container">
+        <div class="footer-item">&copy; 2024 Sattva Space Solutions</div>
+        <div class="footer-item">
+          <a href="mailto:Sattvaspacesolutions@gmail.com"
+            >Sattvaspacesolutions@gmail.com</a
+          >
+        </div>
+        <div class="footer-item">
+          <a
+            href="https://www.instagram.com/sattva_space_solutions"
+            target="_blank"
+            >@sattva_space_solutions</a
+          >
+        </div>
+      </div>
+    </footer>
 </body>
 </html>
+
